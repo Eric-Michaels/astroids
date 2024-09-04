0d09143ec27b40e96f28904bd4b2ee689aa88395
Eric <ericmichaels@gmail.com>
2024-09-04 10:05:12 -0700

    added asteroid split method, when shot if asteroid is above min size it will split and if under min size it is removed

--- asteroid.py
+++ asteroid.py
@@ -5,6 +5,7 @@ from circleshape import CircleShape
 
 
 class Asteroid(CircleShape):
+    containers = ()
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
@@ -13,3 +14,23 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        new_radius = max(self.radius - ASTEROID_MIN_RADIUS, ASTEROID_MIN_RADIUS)
+
+        for _ in range(2):
+            new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            # generate random angle
+            angle = int(random.uniform(20, 50))
+            # rotate the velocity vector by the random angle
+            new_velocity = self.velocity.rotate(random.randint(-angle, angle))
+            new_asteroid.velocity = new_velocity * 1.2
+            new_asteroid.add(self.containers)
+            
+
+
+        
