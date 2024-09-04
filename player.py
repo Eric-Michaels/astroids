14cd8d7439c8680373dbfdb89d29b67f3dcba2b4
Eric <ericmichaels@gmail.com>
2024-09-03 17:19:03 -0700

    added player rotation left and right using 'a' & 'd' keys

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import *
-from constants import PLAYER_RADIUS
+from constants import *
+from main import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -17,3 +18,14 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, (255,255,255), self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
